385f9367b6dab167c13d2bc1b08fd20824d346dc
Nyae44 <tingadaley@gmail.com>
2024-09-04 10:17:22 +0300

    add static configurations and models for library management

--- mlibrary/settings.py
+++ mlibrary/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
